963badd82f0a493704ad7f03d33cb67ff0a7a1d6
shamil-khan <shamil.ahmed@gmail.com>
2025-12-17 17:51:34 +0500

    added @block registry in components.json and added @blocks/login-02
    - <bun shadcn add @blocks/login-02>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import './App.css';
+import Login02 from './components/login-02';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className='text-red-600'>Hello ShadCN</h1>
+      <h1 className='text-red-600'>Hello ShadCN + Block.so</h1>
       <div className='card'>
+        <Login02 />
         <button
           onClick={() => setCount((count) => count + 1)}
           className='px-4 py-2 font-bold text-white bg-blue-600 rounded hover:bg-blue-700'>
