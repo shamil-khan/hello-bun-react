3cd3ae34420465b214a3797ffbc6a15fd8cb9ec9
shamil-khan <shamil.ahmed@gmail.com>
2025-12-17 18:01:42 +0500

    added block.so form-layout blocks - <bun shadcn add @blocks/form-layout-01> - <bun shadcn add @blocks/form-layout-02> - <bun shadcn add @blocks/form-layout-03> - <bun shadcn add @blocks/form-layout-04> - <bun shadcn add @blocks/form-layout-05>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { useState } from 'react';
 import './App.css';
-import Login02 from './components/login-02';
+import Login02 from '@/components/login-02';
+import FormLayout01 from '@/components/form-layout-01';
+import FormLayout02 from '@/components/form-layout-02';
+import FormLayout03 from '@/components/form-layout-03';
+import FormLayout04 from '@/components/form-layout-04';
+import FormLayout05 from '@/components/form-layout-05';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +15,11 @@ function App() {
       <h1 className='text-red-600'>Hello ShadCN + Block.so</h1>
       <div className='card'>
         <Login02 />
+        <FormLayout01 />
+        <FormLayout02 />
+        <FormLayout03 />
+        <FormLayout04 />
+        <FormLayout05 />
         <button
           onClick={() => setCount((count) => count + 1)}
           className='px-4 py-2 font-bold text-white bg-blue-600 rounded hover:bg-blue-700'>
